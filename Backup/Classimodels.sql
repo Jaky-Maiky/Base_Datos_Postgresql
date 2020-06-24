017c84a1f2f08756d3c289a12c652d67396656fe
Jhonny Guevara <jhonny.guevara@pucese.edu.ec>
2020-06-24 14:56:43 -0500

    Postgres

---
+++ Backup/Classimodels.sql
@@ -0,0 +1,43 @@
+-- --------------------------------------------------------
+-- Host:                         jhonnyguevara.me
+-- Versión del servidor:         PostgreSQL 12.3 (Debian 12.3-1.pgdg100+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 8.3.0-6) 8.3.0, 64-bit
+-- SO del servidor:              
+-- HeidiSQL Versión:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES  */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+
+-- Volcando estructura de base de datos para public
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
